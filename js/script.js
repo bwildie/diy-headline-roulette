f960ab7c3876a0dae8a4f677d77a9d3e141e35b1
bwildie <bonwildie@gmail.com>
2017-04-19 13:08:41 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(function(){
     var apiQuery = 'feminists';
     // Words you want to search for -- separate multiple values with spaces, eg:
     // var keywords = 'weather wragge';
-    var keywords = 'all';
+    var keywords = 'any';
     // How you want to combine keywords -- all, any, or phrase
     var keywordType = 'all'
     // Newspaper id numbers -- separate multiple values with spaces, eg:
